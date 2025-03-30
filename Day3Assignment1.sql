7665b6e5dc3509375ff55f6b7e9ddba4a4675d7a
meta-siddhi-agarwal <siddhi.agarwal@metacube.com>
2025-03-31 00:28:53 +0530

    Add files via upload

---
+++ Day3Assignment1.sql
@@ -0,0 +1,21 @@
+USE StoreFont;
+
+SHOW Tables;
+
+DESC Address;
+
+-- list of constraints in address table
+-- 1).ShopperId-Foreign key
+-- 2).AddressLine-Unique key
+-- 3).City-NOT NULL
+-- 4).State-NOT NULL
+-- 5).Country-NOT NULL
+-- 6).Pin code-NOT NULL
+
+DESC Admin;
+
+-- list of constraints in address table
+-- 1).Adminid-primary key
+-- 2).name-not null
+
+
