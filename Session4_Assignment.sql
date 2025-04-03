8094e819a897ad314e8e5a40df2701e7669663b7
meta-siddhi-agarwal <siddhi.agarwal@metacube.com>
2025-04-03 12:59:38 +0530

    Session4_Assignment.sql

--- Session4_Assignment.sql
+++ Session4_Assignment.sql
@@ -4,26 +4,27 @@ USE StoreFont;
 
 #function to cal. no. of orders in a month
 DELIMITER $$
-CREATE FUNCTION Cal_No_Of_Orders_In_Month(Month_Order DATE, Year_Order DATE)
+CREATE FUNCTION Cal_No_Of_Orders_In_Month(Month_Order INT, Year_Order INT)
 RETURNS INT
-NOT DETERMINISTIC
+DETERMINISTIC
 BEGIN
 DECLARE No_Of_Orders INT;
 SELECT COUNT(OrderId) INTO No_Of_Orders FROM ORDERS
-GROUP BY Month_Order(Year_Order(OrderDate)) 
-ORDER BY Month_Order(Year_Order(OrderDate));
+WHERE MONTH(orderdate) = Month_Order 
+AND YEAR(orderdate) = Year_Order
+GROUP BY MONTH(orderdate)
+ORDER BY MONTH(orderdate);
 RETURN No_Of_Orders;
 END $$
 DELIMITER;
 
-
 #function for cal. month which have the max orders provided year
 DELIMITER $$
-CREATE FUNCTION Month_Max_Orders(Year_Max_Order DATE)
-RETURNS DATE
-NOT DETERMINISTIC
+CREATE FUNCTION Month_Max_Orders(Year_Max_Order INT)
+RETURNS INT
+DETERMINISTIC
 BEGIN
-DECLARE Month_Max_Order DATE;
+DECLARE Month_Max_Order INT;
 SELECT MONTH(OrderDate) INTO Month_Max_Order
 FROM ORDERS
 WHERE YEAR(OrderDate)=Year_Max_Order
@@ -33,4 +34,3 @@ LIMIT 1;
 RETURN Month_Max_Order;
 END $$
 DELIMITER ;
-
